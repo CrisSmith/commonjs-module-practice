dd63b27955344b4590bea87a4d815e9259f378ef
Cris Smith <cris.smith.fa@gmail.com>
2019-06-26 19:55:17 -0500

    added files

---
+++ modules/subtractNumbers.js
@@ -0,0 +1,12 @@
+const subtract = require("./subtract");
+
+module.exports.subtractTen =  function(num) {
+    return subtract(num,10);
+  }
+  
+  module.exports.subtractFive = function(num) {
+    return subtract(num, 5);
+  }
+  module.exports.subtractOne = function(num) {
+    return subtract(num, 1);
+  }
